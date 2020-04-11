709e508de873a957a6b351a7bd2e69b5ffe4820c
Tugume Andrew <andrewtugume2@gmail.com>
2020-04-11 04:27:56 +0300

    finished challenge 1

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,54 @@
-const covid19ImpactEstimator = (data) => data;
 
-export default covid19ImpactEstimator;
+const covid19ImpactEstimator = (data) => {
+    data = { // eslint-disable-line no-param-reassign
+      region: {
+        name: 'Africa',
+        avgAge: 19.7,
+        avgDailyIncomeInUSD: 5,
+        avgDailyIncomePopulation: 0.71,
+      },
+      periodType: 'days',
+      timeToElapse: 58,
+      reportedCases: 674,
+      population: 66622705,
+      totalHospitalBeds: 1380614,
+    };
+  
+    const normalisedDuration = (timeToElapse, periodType) => {
+      switch (periodType) {
+        case 'weeks':
+          return timeToElapse * 7;
+        case 'months':
+          return timeToElapse * 30;
+        default:
+          return timeToElapse;
+      }
+    };
+    const days = normalisedDuration(data.timeToElapse, data.periodType);
+  
+    const IpctCurrentlyInfected = data.reportedCases * 10;
+    const IpctInfectionsByRequestedTime = Math.trunc(IpctCurrentlyInfected * (2 ** (days / 3)));
+  
+  
+    const SvrIpctCurrentlyInfected = data.reportedCases * 50;
+    const SvrIpctInfectionsByRequestedTime = Math.trunc(SvrIpctCurrentlyInfected * (2 ** (days / 3)));
+  
+  
+    const bestCaseEstimation = {
+      currentlyInfected: IpctCurrentlyInfected,
+      infectionsByRequestedTime: IpctInfectionsByRequestedTime,
+    };
+    const severeCaseEstimation = {
+      currentlyInfected: SvrIpctCurrentlyInfected,
+      infectionsByRequestedTime: SvrIpctInfectionsByRequestedTime,
+    };
+    return {
+      data,
+      estimate: {
+        impact: bestCaseEstimation,
+        severeImpact: severeCaseEstimation,
+      },
+    };
+  };
+  export default covid19ImpactEstimator;
+  
