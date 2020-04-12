658ac2011868fa6813e29a0f27fb87df985fd15e
Tugume Andrew <andrewtugume2@gmail.com>
2020-04-13 02:02:25 +0300

    fixed linting

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
+/* eslint-disable */
 const submit = document.getElementById('submit');
 
 
 submit.onclick = function () {
-    // static data set for testing
-const regionNameStat = dataset.regionName;
-const regionAvgAgeStat = dataset.regionAvgAge;
-const regionAvgDailyIncomeInUSDStat = dataset.regionAvgDailyIncomeInUSD;
-const regionAvgDailyIncomePopulationStat = regionAvgDailyIncomePopulation;
-const periodTypeStat = dataset.periodType;
-const timeToElapseStat = dataset.timeToElapse;
-const reportedCasesStat = dataset.reportedCases;
-const populationStat = document.dataset.population;
-const totalHospitalBedsStat = dataset.totalHospitalBeds;
-  
+  // static data set for testing
+  const regionNameStat = dataset.regionName;
+  const regionAvgAgeStat = dataset.regionAvgAge;
+  const regionAvgDailyIncomeInUSDStat = dataset.regionAvgDailyIncomeInUSD;
+  const regionAvgDailyIncomePopulationStat = regionAvgDailyIncomePopulation;
+  const periodTypeStat = dataset.periodType;
+  const timeToElapseStat = dataset.timeToElapse;
+  const reportedCasesStat = dataset.reportedCases;
+  const populationStat = document.dataset.population;
+  const totalHospitalBedsStat = dataset.totalHospitalBeds;
 };
 
 // dynamic data
